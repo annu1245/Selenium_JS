3424aef6d8d61a2fd4b082c9b83497794175264d
Annu <annupoddar444@gmail.com>
2021-12-11 12:24:22 +0530

    fetch all links of sports

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ var driver = new webdriver.Builder().forBrowser('chrome').build();
 
 async function Sports() {
     await driver.get('https://sports.ndtv.com/cricket#pfrom=gadgets-globalnav');
+    driver.findElements(By.xpath("*//h3[@class = 'crd_ttl']/a"))
+    .then(res => {
+        var links = res.map(aTag => aTag.getAttribute('href'))
+        Promise.all(links)
+        .then(allLinks => {
+            allLinks.forEach((link) => {
+                console.log(link)
+            })
+        })
+    })
 }
 
 Sports();
